5abb82db29996adb7553f0133803d2b5baf2379f
Thijs Haker <theage2001@gmail.com>
2025-04-09 10:27:27 +0000

    Implemented ticket requesting controller

---
+++ src/main/java/nl/lunarflow/controllers/Request.java
@@ -0,0 +1,23 @@
+package nl.lunarflow.controllers;
+
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import nl.lunarflow.models.Config;
+import nl.lunarflow.models.Ticket;
+import nl.lunarflow.services.*;
+
+// Request a ticket: Either return a filled out ticket, or crash
+
+@Path("/api/request")
+public class Request {
+    @GET
+    public Response requestTicket(Ticket ticket) {
+        try {
+            ticket = BaseService.reqTicket(new Config(), ticket, new GitlabService());
+        } catch (Exception e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(e.toString()).build();
+        }
+        return Response.ok(ticket).build();
+    }
+}
