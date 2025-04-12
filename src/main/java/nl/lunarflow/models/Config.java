557e8e61764cdb4a74d67b5546184231870ed4c7
Thijs Haker <theage2001@gmail.com>
2025-04-12 10:07:35 +0000

    Rewrote envvar fetching

--- src/main/java/nl/lunarflow/models/Config.java
+++ src/main/java/nl/lunarflow/models/Config.java
@@ -1,16 +1,17 @@
 package nl.lunarflow.models;
 
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 public class Config {
-    @ConfigProperty(name = "GITLAB_SERVER")
     public String serverURL;
 
-    @ConfigProperty(name = "GITLAB_PROJECT")
     public String projectPath;
 
-    @ConfigProperty(name = "GITLAB_TOKEN")
     public String token;
+
+    public Config() {
+        this.serverURL = System.getenv("GITLAB_SERVER");
+        this.projectPath = System.getenv("GITLAB_PROJECT");
+        this.token = System.getenv("GITLAB_TOKEN");
+    }
 
     @Override
     public String toString() {
