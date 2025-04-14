d4f670b54fac7170f379346b5513f369c9def0cd
Thijs Haker <theage2001@gmail.com>
2025-04-14 10:53:45 +0000

    Implement demo request

---
+++ src/main/java/nl/lunarflow/controllers/RequestDemo.java
@@ -0,0 +1,22 @@
+package nl.lunarflow.controllers;
+
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.GET;
+import nl.lunarflow.models.Ticket;
+import nl.lunarflow.services.*;
+
+// Request a ticket: Either return a filled out ticket, or crash
+
+@Path("/demo/request")
+public class RequestDemo {
+    @GET
+    public Response requestTicket(Ticket ticket) {
+
+        try {
+            return Response.ok(BaseService.reqTicket(null, ticket, new TestService())).build();
+        } catch (Exception err) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(err.toString()).build();
+        }
+    }
+}
