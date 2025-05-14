2ceb394ae86e5563c6f2878cc58e9df098af7d39
Thijs Haker <theage2001@gmail.com>
2025-05-14 15:27:03 +0000

    Reimplemented backend code

--- src/main/java/nl/lunarflow/GitlabService.java
+++ src/main/java/nl/lunarflow/GitlabService.java
@@ -1,8 +1,12 @@
 package nl.lunarflow;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.gitlab4j.api.GitLabApi;
+import org.gitlab4j.api.models.Assignee;
+import org.gitlab4j.api.models.Issue;
+import org.gitlab4j.api.models.Label;
 
 import nl.lunarflow.models.Config;
 import nl.lunarflow.models.Ticket;
@@ -10,5 +14,109 @@ import nl.lunarflow.models.Taglabel;
 import nl.lunarflow.models.Service;
 
 public class GitlabService implements Service {
-    
+
+    static List<Assignee> mapAssignees(List<String> mailList) {
+        List<Assignee> assignList = new ArrayList<Assignee>();
+        if (mailList != null) {
+            for (String s : mailList) {
+                assignList.add(new Assignee().withEmail(s));
+            }
+        }
+        return assignList;
+    }
+
+    static Ticket mapTicket(Issue issue) {
+        Ticket ticket = new Ticket();
+        ticket.id = issue.getIid();
+        ticket.title = issue.getTitle();
+        ticket.desc = issue.getDescription();
+        ticket.url = issue.getWebUrl();
+        ticket.labels = issue.getLabels();
+        if (issue.getDueDate() != null) {
+            ticket.dueDate = issue.getDueDate().toString();
+        } else {
+            ticket.dueDate = "";
+        }
+        return ticket;
+    }
+
+    static List<Taglabel> mapLabels(List<Label> labelList) {
+        List<Taglabel> res = new ArrayList<Taglabel>();
+        for (Label label : labelList) {
+            Taglabel lab = new Taglabel();
+            lab.id = label.getId();
+            lab.name = label.getName();
+            lab.color = label.getColor();
+            res.add(lab);
+        }
+        return res;
+    }
+
+    @Override
+    public Ticket createIssue(Config conf, Ticket ticket) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        Issue issue = api.getIssuesApi().createIssue(conf.projectPath, ticket.title, ticket.desc);
+        issue.setAssignees(mapAssignees(ticket.assignees));
+
+        ticket.id = issue.getIid();
+        ticket.url = issue.getWebUrl();
+        api.close();
+
+        return ticket;
+    }
+
+    @Override
+    public Ticket readIssue(Config conf, Ticket ticket) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        Issue issue = api.getIssuesApi().getIssue(conf.projectPath, ticket.id);
+        Ticket ret = mapTicket(issue);
+
+        api.close();
+        return ret;
+    }
+
+    @Override
+    public Ticket closeIssue(Config conf, Ticket ticket) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        api.getIssuesApi().closeIssue(conf.projectPath, ticket.id);
+        api.close();
+        return ticket;
+    }
+
+    @Override
+    public Ticket setLabels(Config conf, Ticket ticket) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        Issue issue = api.getIssuesApi().getIssue(conf.projectPath, ticket.id);
+        issue.setLabels(ticket.labels);
+        Ticket ret = mapTicket(issue);
+
+        api.close();
+        return ret;
+    }
+
+    @Override
+    public Taglabel createLabel(Config conf, Taglabel tlabel) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        Label glLabel = api.getLabelsApi().createProjectLabel(conf.projectPath, new Label().withName(tlabel.name));
+        tlabel.id = glLabel.getId();
+        api.close();
+        return tlabel;
+    }
+
+    @Override
+    public Taglabel deleteLabel(Config conf, Taglabel tlabel) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        api.getLabelsApi().deleteProjectLabel(conf.projectPath, tlabel.id);
+        tlabel.id = (long) -1;
+        api.close();
+        return tlabel;
+    }
+
+    @Override
+    public List<Taglabel> listLabels(Config conf) throws Exception {
+        GitLabApi api = new GitLabApi(conf.serverURL, conf.token);
+        var res = mapLabels(api.getLabelsApi().getProjectLabels(conf.projectPath));
+        api.close();
+        return res;
+    }
 }
--- src/main/java/nl/lunarflow/models/Service.java
+++ src/main/java/nl/lunarflow/models/Service.java
@@ -3,12 +3,12 @@ package nl.lunarflow.models;
 import java.util.List;
 
 public interface Service {
-    public Ticket createIssue(Config conf, Ticket issue) throws Exception;
-    public Ticket readIssue(Config conf, Ticket issue) throws Exception;
-    public Ticket closeIssue(Config conf, Ticket issue) throws Exception;
-    public Ticket setLabels(Config conf, Ticket issue) throws Exception;
+    public Ticket createIssue(Config conf, Ticket ticket) throws Exception;
+    public Ticket readIssue(Config conf, Ticket ticket) throws Exception;
+    public Ticket closeIssue(Config conf, Ticket ticket) throws Exception;
+    public Ticket setLabels(Config conf, Ticket ticket) throws Exception;
 
-    public Taglabel createLabel(Config conf, Taglabel label) throws Exception;
-    public Taglabel deleteLabel(Config conf, Taglabel label) throws Exception;
+    public Taglabel createLabel(Config conf, Taglabel tlabel) throws Exception;
+    public Taglabel deleteLabel(Config conf, Taglabel tlabel) throws Exception;
     public List<Taglabel> listLabels(Config conf) throws Exception;
 }
